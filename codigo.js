1ba1bc70a68a80a9974ca38b96afac7db103fbdf
FrancoZvilling <francozvilling2014@hotmail.com>
2023-06-14 22:24:06 -0300

    tercera entrega

--- codigo.js
+++ codigo.js
@@ -1,78 +1,83 @@
-//Variables://
 let jugadorUnot1 = 0;
 let jugadorDost1 = 0;
 let jugadorUnot2 = 0;
 let jugadorDost2 = 0;
 let jugadorUnot3 = 0;
 let jugadorDost3 = 0;
+let contadorJugador1 = 0;
+let contadorJugador2 = 0;
 
-//Arreglos://
+//Arreglos//
 let posicionesJugadorUno = [];
 let posicionesJugadorDos = [];
 
-//Objetos://
-class jugador{
-    constructor(nombre, apellido, edad){
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.edad=edad;
-    }
+//Funciones://
+function turnoJugador1(lista_turno){
+    tablero[lista_turno].addEventListener("click",function(){
+        tablero[lista_turno].style.backgroundColor= "#008000"
+    })
+}
+
+function turnoJugador2(lista_turno){
+    tablero[lista_turno].addEventListener("click",function(){
+        tablero[lista_turno].style.backgroundColor= "#FF0000"
     
+    })
+}
 
-    get_datos(){
-        console.log("Los datos del jugador son: Nombre:", this.nombre,"- Apellido:",this.apellido,"- Edad:", this.edad);
-    }
+function sumarPuntosJugadorUno(){
+    boton[0].addEventListener("click",function(){
+        contadorJugador1=contadorJugador1 + 1
+        console.log("Puntos del jugador 1: ", contadorJugador1)
+        sessionStorage.setItem("Puntos_del_jugador_1: ", contadorJugador1)
+    })
+}
 
-    get_mayor_de_edad(){
-        if(this.edad>17){
-            console.log("El jugador",this.nombre,"es mayor de edad.");
-        }
-
-        else{
-            console.log("El jugador",this.nombre,"es menor de edad.");
-        }
-    }
+function sumarPuntosJugadorDos(){
+    boton[1].addEventListener("click",function(){
+        contadorJugador2=contadorJugador2 + 1
+        console.log("Puntos del jugador 2: ", contadorJugador2)
+        sessionStorage.setItem("Puntos_del_jugador_2: ", contadorJugador2)
+    })
 }
 
 //Codigo://
-let jugadorUno = new jugador("Franco", "Zvilling", 6);
-let jugadorDos = new jugador("Mirta", "Legrand", 1000000);
-jugadorUno.get_datos();
-jugadorUno.get_mayor_de_edad()
-jugadorDos.get_datos()
-jugadorDos.get_mayor_de_edad()
 
 alert("Bienvenido al TA-TE-TI")
 alert("Reglas del juego:")
-alert("Imagine un tablero de 3x3 donde la primera posicion de abajo a la izquierda es la numero 1 y la ultima posicion de arriba a la derecha es la posicion 9.")
-alert("Los jugadores iran ingresando por turnos una posicion del 1 al 9 que sera guardada. El primero en colocar 3 posiciones seguidas tanto en fila, columna o diagonal gana la partida.")
+alert("Imagine un tablero de 3x3 donde la primera posicion de arriba a la izquierda es la numero 0 y la ultima posicion de abajo a la derecha es la posicion 8.")
+alert("Los jugadores iran ingresando por turnos una posicion del 0 al 8 que sera guardada. El primero en colocar 3 posiciones seguidas tanto en fila, columna o diagonal gana la partida.")
+alert("Al final de la partida se le mostrará un tablero en pantalla en el cual haciendo click en las posiciones ya elegidas anteriormente podra ver de color VERDE las posiciones elegidas por el jugador uno y de color ROJO las posiciones elegidas por el jugador 2")
+let tablero = document.getElementsByClassName("caja__hijo");
 
 //Primer turno del jugador 1//
 
 do{
     jugadorUnot1 = prompt("Jugador 1: ingrese su primera posicion:");
-    if(jugadorUnot1>0 && jugadorUnot1<10){
+    if(jugadorUnot1>-1 && jugadorUnot1<9){
         posicionesJugadorUno.push(jugadorUnot1);
+        turnoJugador1(jugadorUnot1)
     }
 
     else{
-        alert("El valor ingresado no es correcto, por favor elija una posicion del 1 al 9")
+        alert("El valor ingresado no es correcto, por favor elija una posicion del 0 al 8")
     }
 }
 
-while(jugadorUnot1<1 || jugadorUnot1>9)
+while(jugadorUnot1<0 || jugadorUnot1>8)
 
 
 //Primer turno del jugador2//
 
 do{
     jugadorDost1 = prompt("Jugador 2: ingrese su primera posicion");
-    if(jugadorDost1 != jugadorUnot1 && jugadorDost1>0 && jugadorDost1<10){
+    if(jugadorDost1 != jugadorUnot1 && jugadorDost1>-1 && jugadorDost1<9){
         posicionesJugadorDos.push(jugadorDost1);
+        turnoJugador2(jugadorDost1)
     }
 
-    else if(jugadorDost1<1 || jugadorDost1>9){
-        alert("El valor ingresado no es correcto, por favor elija una posicion del 1 al 9")
+    else if(jugadorDost1<0 || jugadorDost1>8){
+        alert("El valor ingresado no es correcto, por favor elija una posicion del 0 al 8")
     }
 
     else{
@@ -80,13 +85,14 @@ do{
     }
 }
 
-while(jugadorDost1==jugadorUnot1 || jugadorDost1<1 || jugadorDost1>9);
+while(jugadorDost1==jugadorUnot1 || jugadorDost1<0 || jugadorDost1>8);
 
 //Segundo turno del jugador 1//
 do{
     jugadorUnot2 = prompt("Jugador 1: ingrese su segunda posicion:");
-    if(jugadorDost1 != jugadorUnot2 && jugadorUnot1 != jugadorUnot2 && jugadorUnot2>0 && jugadorUnot2<10){
+    if(jugadorDost1 != jugadorUnot2 && jugadorUnot1 != jugadorUnot2 && jugadorUnot2>-1 && jugadorUnot2<9){
         posicionesJugadorUno.push(jugadorUnot2);
+        turnoJugador1(jugadorUnot2)
     }
 
     else if(jugadorUnot2==jugadorDost1){
@@ -94,8 +100,8 @@ do{
         
     }
 
-    else if(jugadorUnot2<1 || jugadorUnot2>9){
-        alert("El valor ingresado no es correcto, por favor elija una posicion del 1 al 9")
+    else if(jugadorUnot2<0 || jugadorUnot2>8){
+        alert("El valor ingresado no es correcto, por favor elija una posicion del 0 al 8")
     }
 
     else if(jugadorUnot2==jugadorUnot1){
@@ -110,8 +116,9 @@ while(jugadorDost1==jugadorUnot2 || jugadorUnot2 == jugadorUnot1 || jugadorUnot2
 
 do{
     jugadorDost2 = prompt("Jugador 2: ingrese su segunda posicion:");
-    if(jugadorDost2 != jugadorUnot2 && jugadorDost1 != jugadorDost2 && jugadorDost2 != jugadorUnot1 && jugadorDost2>0 && jugadorDost2<10){
+    if(jugadorDost2 != jugadorUnot2 && jugadorDost1 != jugadorDost2 && jugadorDost2 != jugadorUnot1 && jugadorDost2>-1 && jugadorDost2<9){
         posicionesJugadorDos.push(jugadorDost2);
+        turnoJugador2(jugadorDost2)
     }
 
     else if(jugadorUnot2==jugadorDost2){
@@ -119,8 +126,8 @@ do{
         
     }
 
-    else if(jugadorDost2<1 || jugadorDost2>9){
-        alert("El valor ingresado no es correcto, por favor elija una posicion del 1 al 9")
+    else if(jugadorDost2<0 || jugadorDost2>8){
+        alert("El valor ingresado no es correcto, por favor elija una posicion del 0 al 8")
     }
 
     else if(jugadorDost1==jugadorDost2){
@@ -133,14 +140,15 @@ do{
         
     }
 }
-while(jugadorUnot2==jugadorDost2 || jugadorDost1==jugadorDost2 || jugadorDost2==jugadorUnot1 || jugadorDost2<1 || jugadorDost2>9);
+while(jugadorUnot2==jugadorDost2 || jugadorDost1==jugadorDost2 || jugadorDost2==jugadorUnot1 || jugadorDost2<0 || jugadorDost2>8);
 
 //Tercer turno del jugador 1//
 
 do{
     jugadorUnot3 = prompt("Jugador 1: ingrese su tercera posicion:");
-    if(jugadorUnot3 != jugadorUnot1 && jugadorUnot3 != jugadorUnot2 && jugadorUnot3 != jugadorDost1 && jugadorUnot3 != jugadorDost2 && jugadorUnot3>0 && jugadorUnot3<10){
+    if(jugadorUnot3 != jugadorUnot1 && jugadorUnot3 != jugadorUnot2 && jugadorUnot3 != jugadorDost1 && jugadorUnot3 != jugadorDost2 && jugadorUnot3>-1 && jugadorUnot3<9){
         posicionesJugadorUno.push(jugadorUnot3);
+        turnoJugador1(jugadorUnot3)
     }
 
     else if(jugadorUnot3==jugadorUnot1){
@@ -153,8 +161,8 @@ do{
         
     }
 
-    else if(jugadorUnot3<1 || jugadorUnot3>9){
-        alert("El valor ingresado no es correcto, por favor elija una posicion del 1 al 9")
+    else if(jugadorUnot3<0 || jugadorUnot3>8){
+        alert("El valor ingresado no es correcto, por favor elija una posicion del 0 al 8")
     }
 
     else if(jugadorUnot3==jugadorDost1){
@@ -167,27 +175,16 @@ do{
         
     }
 }
-while(jugadorUnot3==jugadorUnot1 || jugadorUnot3==jugadorUnot2 || jugadorUnot3==jugadorDost1 || jugadorUnot3==jugadorDost2 || jugadorUnot3<1 || jugadorUnot3>9);
+while(jugadorUnot3==jugadorUnot1 || jugadorUnot3==jugadorUnot2 || jugadorUnot3==jugadorDost1 || jugadorUnot3==jugadorDost2 || jugadorUnot3<0 || jugadorUnot3>8);
 
-//Jugador uno posible ganador//
-
-//if(posicionesJugadorUno == "1","2","3"){
-    
-    //|| posicionesJugadorUno === "4","5","6"||posicionesJugadorUno === "7","8","9"|| posicionesJugadorUno === "9","8","7" || posicionesJugadorUno==="6","5","4" || 
-    //posicionesJugadorUno==="3","2","1" || posicionesJugadorUno==="1","4","7" || posicionesJugadorUno==="7","4","1" || posicionesJugadorUno==="3","6","9"||posicionesJugadorUno==="9","6","3"||posicionesJugadorUno==="7","5","3"
-    //|| posicionesJugadorUno==="3","5","7"||posicionesJugadorUno==="1","5","9"||posicionesJugadorUno==="9","5","2" || posicionesJugadorUno==="2","5","8"||posicionesJugadorUno==="8","5","2"){
-        
-    //alert("EL JUGADOR 1 HA GANADO: FELICITACIONES!");
-    
-//}
-//else{
 
 //Tercer turno del jugador 2//
 
 do{
     jugadorDost3 = prompt("Jugador 2: ingrese su tercera posicion:");
-    if(jugadorDost3 != jugadorUnot1 && jugadorDost3 != jugadorUnot2 && jugadorDost3 != jugadorUnot3 && jugadorDost3 != jugadorDost2 && jugadorDost3 != jugadorDost1 && jugadorDost3>0 && jugadorDost3<10){
+    if(jugadorDost3 != jugadorUnot1 && jugadorDost3 != jugadorUnot2 && jugadorDost3 != jugadorUnot3 && jugadorDost3 != jugadorDost2 && jugadorDost3 != jugadorDost1 && jugadorDost3>-1 && jugadorDost3<9){
         posicionesJugadorDos.push(jugadorDost3);
+        turnoJugador2(jugadorDost3)
     }
 
     else if(jugadorDost3==jugadorUnot1){
@@ -200,8 +197,8 @@ do{
         
     }
 
-    else if(jugadorDost3<1 || jugadorDost3>9){
-        alert("El valor ingresado no es correcto, por favor elija una posicion del 1 al 9")
+    else if(jugadorDost3<0 || jugadorDost3>8){
+        alert("El valor ingresado no es correcto, por favor elija una posicion del 0 al 8")
     }
 
     else if(jugadorDost3==jugadorUnot3){
@@ -219,21 +216,15 @@ do{
         
     }
 }
-while(jugadorDost3==jugadorUnot1 || jugadorDost3==jugadorUnot2 || jugadorDost3==jugadorUnot3 || jugadorDost3==jugadorDost1 || jugadorDost3==jugadorDost2 || jugadorDost3<1 || jugadorDost3>9);
+while(jugadorDost3==jugadorUnot1 || jugadorDost3==jugadorUnot2 || jugadorDost3==jugadorUnot3 || jugadorDost3==jugadorDost1 || jugadorDost3==jugadorDost2 || jugadorDost3<0 || jugadorDost3>8);
 
-//Jugador dos posible ganador//
+//SUMATORIA DE PUNTOS MANUAL//
 
-//if(posicionesJugadorDos == "1","2","3"|| posicionesJugadorDos == "4","5","6"||posicionesJugadorDos == "7","8","9"|| posicionesJugadorDos == "9","8","7" || posicionesJugadorDos=="6","5","4" || 
- //   posicionesJugadorDos=="3","2","1" || posicionesJugadorDos=="1","4","7" || posicionesJugadorDos=="7","4","1" || posicionesJugadorDos=="3","6","9"||posicionesJugadorDos=="9","6","3"||posicionesJugadorDos=="7","5","3"
-  //  || posicionesJugadorDos=="3","5","7"||posicionesJugadorDos=="1","5","9"||posicionesJugadorDos=="9","5","2" || posicionesJugadorDos=="2","5","8"||posicionesJugadorDos=="8","5","2"){
-  //  alert("EL JUGADOR 2 HA GANADO: FELICITACIONES!")
-    
-//}
+let boton = document.getElementsByClassName("boton")
 
-//else{
-   // alert("Han empatado");
-//}
-//}
+sumarPuntosJugadorUno()
+sumarPuntosJugadorDos()
+
 
 
 
